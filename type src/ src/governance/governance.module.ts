e58c24be76bee3263dd5bca8396a01a2c322e2a9
MarkLLL0708 <marklyy0708@gmail.com>
2026-03-16 14:07:46 +0800

    Update governance.module.ts

--- type src/ src/governance/governance.module.ts
+++ type src/ src/governance/governance.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GovernanceController } from './governance.controller';
+import { GovernanceService } from './governance.service';
 
-@Module({})
+@Module({
+  controllers: [GovernanceController],
+  providers: [GovernanceService],
+  exports: [GovernanceService],
+})
 export class GovernanceModule {}
